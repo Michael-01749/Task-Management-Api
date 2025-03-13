623c24901507bb6d738912d247ba188dcca3dba7
Hot-bo <innocentmichael32@.com>
2025-03-13 18:12:36 +0100

    first commit

--- db.js
+++ db.js
@@ -3,16 +3,16 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MongoUrl, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+  try {
+    await mongoose.connect(process.env.MongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log("✅ Connected to MongoDB database");
-    } catch (error) {
-        console.error("❌ Error connecting to MongoDB:", error.message);
-    }
+    console.log("✅ Connected to MongoDB database");
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:");
+  }
 };
 
 module.exports = connectDB;
