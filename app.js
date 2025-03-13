5612a630426560199177aeabbfa91baf56a5dba9
Hot-bo <innocentmichael32@.com>
2025-03-13 20:15:11 +0100

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();               
 const taskRoutes = require("./routes/taskRoute.js");
 const connectDB = require('./db');
+const PORT = process.env.PORT || 3000;
               
 
 // Middleware
@@ -12,7 +13,7 @@ app.get('/', (req, res) => {
     res.send("Welcome to our server");
 });
 
-const PORT = process.env.PORT
+// const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
