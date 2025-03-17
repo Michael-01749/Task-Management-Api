67fb8fc18fc685e66c8d262e80f88b0d79be877e
Hot-bo <innocentmichael32@.com>
2025-03-17 18:33:13 +0100

    aaa

--- db.js
+++ db.js
@@ -3,17 +3,19 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  console.log("MongoUrl Gotten: ",process.env.MongoUrl)
+  
   try {
     await mongoose.connect(process.env.MongoUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoUrl: ",process.env.MongoUrl)
-
+    
     console.log("✅ Connected to MongoDB database");
   } catch (error) {
     console.error("❌ Error connecting to MongoDB:");
   }
+  
 };
 
 module.exports = connectDB;
